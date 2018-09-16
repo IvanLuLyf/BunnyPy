650a849af34e175204f200b4dc3b3184ef388536
IvanLuLyf <835498692@qq.com>
2018-09-17 00:21:22 +0800

    add get query string and request body

--- app.py
+++ app.py
@@ -8,5 +8,21 @@ def index():
     return "<h1>Hello World</h1>"
 
 
+@app.route('/hi')
+def hi():
+    if app.request_method == 'GET':
+        return '''
+        <form action="hi" method="POST">
+            <input type="text" name="name">
+            <input type="submit" value="Submit">
+        </form>
+        '''
+    else:
+        name = app.request("name", method='POST')
+        if name:
+            return "<h1>Hi!" + name + "</h1>"
+        return "<h1>No Data</h1>"
+
+
 if __name__ == '__main__':
     app.run()
--- cobra.py
+++ cobra.py
@@ -1,40 +1,77 @@
 from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs, unquote
+from html import escape
 
 
 class Cobra:
-    rules = []
+    __rules = []
+    __queries = {}
+    __request_bodies = {}
+    request_method = 'GET'
 
     def __init__(self, host='127.0.0.1', port=8000):
-        self.host = host
-        self.port = port
+        self.__host = host
+        self.__port = port
 
-    def application(self, environ, start_response):
+    def __parse_input(self, environ):
+        try:
+            content_length = int(environ['CONTENT_LENGTH'])
+        except ValueError:
+            content_length = 0
+        print(environ)
+        request_body = environ['wsgi.input'].read(content_length)
+        self.__request_bodies = parse_qs(request_body.decode('utf-8'),)
+
+    def request(self, name, method='GET', multi_param=False):
+        if method == 'GET':
+            if multi_param:
+                data_arr = self.__queries.get(name, [])
+                return [escape(data) for data in data_arr]
+            data = self.__queries.get(name, [None])[0]
+            if data:
+                return escape(data)
+            return None
+        else:
+            if multi_param:
+                data_arr = self.__request_bodies.get(name, [])
+                return [escape(data) for data in data_arr]
+            data = self.__request_bodies.get(name, [None])[0]
+            if data:
+                return escape(unquote(data))
+            return None
+
+    def __application(self, environ, start_response):
         url = environ['PATH_INFO']
+        query_string = environ['QUERY_STRING']
+        self.request_method = environ['REQUEST_METHOD']
+        if self.request_method != 'GET':
+            self.__parse_input(environ)
+        self.__queries = parse_qs(query_string)
         if url.startswith("/static/"):
             try:
                 with open(url[1:], "rb") as static_file:
                     data = static_file.read()
             except FileNotFoundError:
                 data = b'<h1>404 Not Found</h1>'
-            start_response('200 OK', [('Content-Type', 'text/html')])
+            start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
             return [data]
-        for rule in self.rules:
+        for rule in self.__rules:
             if rule['path'] == url:
                 resp = rule['func']()
-                start_response('200 OK', [('Content-Type', 'text/html')])
+                start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
                 return [resp.encode('utf-8')]
-        start_response('200 OK', [('Content-Type', 'text/html')])
+        start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
         return [b'<h1>Welcome to use cobra.py!</h1>', b'<p>cobra.py is successfully working.</p>']
 
     def route(self, path, **options):
         def decorator(func):
-            self.rules.append({'path': path, 'func': func})
+            self.__rules.append({'path': path, 'func': func})
             return func
 
         return decorator
 
     def run(self):
-        httpd = make_server(self.host, self.port, self.application)
-        print("Serving HTTP on port {0}...".format(self.port))
-        print("Running on http://{0}:{1}/ (Press CTRL+C to quit)".format(self.host, self.port))
+        httpd = make_server(self.__host, self.__port, self.__application)
+        print("Serving HTTP on port {0}...".format(self.__port))
+        print("Running on http://{0}:{1}/ (Press CTRL+C to quit)".format(self.__host, self.__port))
         httpd.serve_forever()
