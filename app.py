87d48d91e9b7a7fc62a8e1ee8f185cff50cb4fdd
IvanLuLyf <835498692@qq.com>
2018-09-18 00:32:57 +0800

    Add Database Select and Insert Example

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from cobra import Cobra
+import sqlite3
 
-app = Cobra()
+app = Cobra(connection=sqlite3.connect('test.db'))
 
 
 @app.route('/index')
@@ -22,6 +23,29 @@ def hi():
         if name:
             return "<h1>Hi!" + name + "</h1>"
         return "<h1>No Data</h1>"
+
+
+@app.route("/users")
+def db():
+    @app.select("select * from user", fetch_all=True)
+    def get_all():
+        pass
+
+    @app.insert("insert into user (name) values (?)")
+    def insert_user(name):
+        pass
+
+    name = app.request("name", method='POST')
+    if name:
+        insert_user(name)
+    data = get_all()
+    l = '<br>'.join([str(d) for d in data])
+    return l + '''
+    <form action="users" method="POST">
+        <input type="text" name="name">
+        <input type="submit" value="Submit">
+    </form>
+    '''
 
 
 if __name__ == '__main__':
