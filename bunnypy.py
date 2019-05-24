089086780777d37cdfbb9b5b5faa9b063c08b110
IvanLuLyf <835498692@qq.com>
2019-05-25 00:53:48 +0800

    Update BunnyPy Logo

--- bunnypy.py
+++ bunnypy.py
@@ -84,7 +84,7 @@ class Bunny:
 | __ -| | |   |   | | |   __| | |
 |_____|___|_|_|_|_|_  |__|  |_  |
                   |___|     |___|
-BunnyPy v0.1.3
+BunnyPy v0.1.4
 Serving HTTP on port {1}...
 Running on http://{0}:{1}/ (Press CTRL+C to quit)
 '''
