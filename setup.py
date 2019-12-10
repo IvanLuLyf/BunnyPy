144af6fa0b059d972036b04e1977f804e924c153
IvanLuLyf <835498692@qq.com>
2019-12-11 01:25:34 +0800

    Update 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='BunnyPy',
     version='0.1.7',
     description='Lightweight Python Web Framework',
-    long_description=open('README.md').read(),
+    long_description=open('README.md',encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     author='IvanLuLyf',
     author_email='me@ivanlu.cn',
